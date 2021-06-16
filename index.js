57fb6bfdd7b1e7617d8d5b0e9afcbc45cd84ed21
caetanomarcus <caetanomarcusfgmail.com>
2021-06-16 12:01:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const buttonYes = () => {
+    const header = document.querySelector('.header');
+    const main = document.querySelector('main')
+    header.style.display = 'none';
+    main.style.display = 'flex'
+}
+
+const buttonNo = () => {
+    if(confirm("Tem certeza que não quer sair comigo, Michelle?")){
+        alert ("Ok, bye")
+    }
+}
+
+const buttonMall = () => {
+    const header = document.querySelector('.shopping');
+    const main = document.querySelector('main')
+    header.style.display = 'flex';
+    main.style.display = 'none'
+}
+
+const buttonBeach = () => {
+    const header = document.querySelector('.praia');
+    const main = document.querySelector('main')
+    header.style.display = 'flex';
+    main.style.display = 'none'
+}
+
+const buttonBar = () => {
+    const header = document.querySelector('.bar');
+    const main = document.querySelector('main')
+    header.style.display = 'flex';
+    main.style.display = 'none'
+}
